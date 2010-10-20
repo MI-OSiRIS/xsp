32a5107e85a84b37004acf53eae633fd8c40dfd9
swany <swany@6dc24a1a-a86e-4cf8-a923-33330a3f456b>
2010-10-20 01:41:04 +0000

    compilation on the older sdk
    git-svn-id: https://damsl.cs.indiana.edu/svn/xsp/trunk@53 6dc24a1a-a86e-4cf8-a923-33330a3f456b

--- xspd/do-link.sh
+++ xspd/do-link.sh
@@ -5,6 +5,10 @@ do
 ln $i `echo $i | awk -F / '{print $3}'`
 done
 
+ln ../include/xsp-proto.h .
+
+ln ../libconfig/config.tab.h .
+
 ln ../compat/compat.c compat.c
 ln ../compat/compat.h compat.h
 ln ../compat/queue.h queue.h
@@ -15,3 +19,5 @@ ln ../libconfig/libconfig.h libconfig.h
 ln ../libconfig/private.h private.h
 ln ../libconfig/scanner.c scanner.c
 ln ../libconfig/scanner.h scanner.h
+
+ln ../include/config.h.junos config.h
