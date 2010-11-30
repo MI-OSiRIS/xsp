2d1626a0f13790590b88a7bd8178f3a7a54650e6
kissel <kissel@6dc24a1a-a86e-4cf8-a923-33330a3f456b>
2010-11-30 19:08:05 +0000

    allows monitoring lib and client to update all monitored path fields
    git-svn-id: https://damsl.cs.indiana.edu/svn/xsp/trunk@54 6dc24a1a-a86e-4cf8-a923-33330a3f456b

--- modules/monitoring/client.c
+++ modules/monitoring/client.c
@@ -9,8 +9,8 @@ int main()
   struct soap soap;
   xspdSoapContext mntr;
 
-  char *endpoint = "http://blackseal.damsl.cis.udel.edu/escpscope/monitor-service/?service";
-  //char *endpoint = "http://192.168.1.20:8000/monitor-service/?service";	
+  //char *endpoint = "http://blackseal.damsl.cis.udel.edu/escpscope/monitor-service/?service";
+  char *endpoint = "http://192.168.1.20:8000/monitor-service/?service";	
 
   uint64_t start_time = 1234567890;
   uint64_t duration = 600;
@@ -24,7 +24,7 @@ int main()
   mntr.soap_action=NULL;
   
   printf("Signaling for new path reservation\n");
-  if(monitoring_notify(&mntr,"123123124","udel","bnl","1000-2000","1000-2000",
+  if(monitoring_notify(&mntr,"123123124","198.124.220.194","198.124.220.137","1000-2000","1000-3000",
 		       "3019","sender", start_time, duration, bw, "EF", "pending") == 0)  
     printf("Signaling is done for notification of path reservation\n");    
   else
@@ -44,14 +44,23 @@ int main()
   
   sleep(10);
 
+  printf("Signaling for update of path reservation\n");
+  if(monitoring_update_path(&mntr,"123123124", NULL, NULL, "5000", "2000", "4567",
+			    "bidirectional", 0, 9000, 34000, "EK", "active")==0)
+	  printf("Signaling is done for update of path reservation\n");
+  else
+	  printf("Signaling failed for update of path reservation\n");
+
+  printf("\n------------------------------------------------------------------\n");
+
+  sleep(10);
+
   printf("Signaling for removed path reservation\n");  
   if(monitoring_remove(&mntr,"123123124")==0)
     printf("Signaling is done for removal of path reservation\n");    
   else
     printf("Signaling failed for removal of path reservation\n"); 
   
-  sleep(10);
-
   soap_destroy(&soap); 
   soap_end(&soap); 
   soap_done(&soap); 
--- modules/monitoring/monitoring.h
+++ modules/monitoring/monitoring.h
@@ -12,6 +12,11 @@ int monitoring_notify(xspdSoapContext *mntr, const char *id, const char *src,
 
 int monitoring_set_status(xspdSoapContext *mntr, const char *res_id, const char *status);
 
+int monitoring_update_path(xspdSoapContext *mntr, const char *res_id, const char *src,
+                           const char *dst, const char *src_port_range, const char *dst_port_range,
+                           const char *vlan_id, const char *direction, uint64_t start_time,
+                           uint64_t duration, uint64_t bw, const char *bw_class, const char *status);
+
 int monitoring_remove(xspdSoapContext *mntr, const char *res_id);
 
 #endif
