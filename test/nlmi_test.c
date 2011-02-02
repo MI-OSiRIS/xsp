caa6b59f53f2020fcab4c3a49eee7bf271d3c380
fernandes <fernandes@6dc24a1a-a86e-4cf8-a923-33330a3f456b>
2011-02-02 04:29:08 +0000

    adding a nlmi bson test message
    git-svn-id: https://damsl.cis.udel.edu/svn/xsp/trunk@86 6dc24a1a-a86e-4cf8-a923-33330a3f456b

--- test/nlmi_test.c
+++ test/nlmi_test.c
@@ -48,14 +48,78 @@ int main(int argc, char *argv[])
 	bson_buffer_init(&bb);
 	bson_ensure_space(&bb, 131072);
 	
-	bson_append_start_object(&bb, "subject");
-	bson_append_start_array(&bb, "host");
+	/*
+	 *   "version" : [ 0, 0, 0 ],
+	 */
+	bson_append_start_array(&bb, "version");
+	    bson_append_int(&bb, "", 0);
+	    bson_append_int(&bb, "", 0);
+	    bson_append_int(&bb, "", 0);
+	bson_append_finish_object(&bb);
 
-	for(i=0; i <= 5; i++) {
-		bson_append_string(&bb, "", "test");
-	}
-	bson_append_finish_object(&bb); /* [host] */
-	bson_append_finish_object(&bb); /* {subject} */
+
+	/*
+	 *   "subject" : {
+     *         "_id": "sub1",
+     *         "node": {
+     *                 "hostName": "test.our.edu",
+     *         },
+     *   },
+    */
+	bson_append_start_object(&bb, "subject");
+	    bson_append_string(&bb, "_id", "sub1");
+	    bson_append_start_object(&bb, "node");
+	        bson_append_string(&bb, "hostName", "test.our.edu");
+	    bson_append_finish_object(&bb);
+	bson_append_finish_object(&bb);
+
+
+	/*   "param" : {
+     *         "_id" : "param1",
+     *   },
+     */
+	bson_append_start_object(&bb, "param");
+	    bson_append_string(&bb, "_id", "param1");
+	bson_append_finish_object(&bb);
+
+
+	/*
+	 *  "data": {
+     *          "http://ggf.org/ns/nmwg/characteristic/utilization/2.0" : {
+     *              "ts" : 1282786560,
+     *              "n" : 10,
+     *              "dt" : 5,
+     *              "values" : {
+     *                  "value" : [ 5, 7, 8, 3, 2, 10, 11, 20, 25, 21, ],
+     *                  "valueUnits" : [
+     *                          "Mbps", "Mbps", "Mbps", "Mbps", "Mbps",
+     *                          "Mbps", "Mbps", "Mbps", "Mbps", "Mbps",
+     *                  ],
+     *               }
+     *          }
+     *   }
+	 */
+	bson_append_start_object(&bb, "data");
+        bson_append_start_object(&bb,
+                "http://ggf.org/ns/nmwg/characteristic/utilization/2.0");
+            bson_append_int(&bb, "ts", 1282786560);
+            bson_append_int(&bb, "n", 10);
+            bson_append_int(&bb, "dt", 5);
+            bson_append_start_object(&bb, "values");
+                bson_append_start_array(&bb, "value");
+                {
+                    int value[] = { 5, 7, 8, 3, 2, 10, 11, 20, 25, 21 };
+                    for (i = 0; i < 10; i++)
+                        bson_append_int(&bb, "", value[i]);
+                }
+                bson_append_finish_object(&bb);
+                bson_append_start_array(&bb, "valueUnis");
+                    for (i = 0; i < 10; i++)
+                        bson_append_string(&bb, "", "Mbps");
+                bson_append_finish_object(&bb);
+            bson_append_finish_object(&bb);
+        bson_append_finish_object(&bb);
+    bson_append_finish_object(&bb);
 
 	bson_from_buffer(&b, &bb);
 	bsz = bson_size(&b);
