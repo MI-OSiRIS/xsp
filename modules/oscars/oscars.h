2ba6f59f8d1fe30923b2b930f31dabca9d25636a
kissel <kissel@6dc24a1a-a86e-4cf8-a923-33330a3f456b>
2011-03-28 18:47:39 +0000

    refactoring gsoap, fixes for oscars/wsse
    git-svn-id: https://damsl.cs.indiana.edu/svn/xsp/trunk@113 6dc24a1a-a86e-4cf8-a923-33330a3f456b

--- modules/oscars/oscars.h
+++ modules/oscars/oscars.h
@@ -3,7 +3,10 @@
 
 #include <stdint.h>
 #include "xspd_soap_context.h"
+#include "threads.h"
+#include "wsseapi.h"
 
 int oscars_cancelReservation(xspdSoapContext *osc, const char *gri, char **res);
+
 
 #endif
