80b2f7bef75598c1366ab8753e701b3ce1caf1b5
mzhang <mzhang@6dc24a1a-a86e-4cf8-a923-33330a3f456b>
2011-10-01 00:18:04 +0000

    git-svn-id: https://damsl.cis.udel.edu/svn/xsp/trunk@193 6dc24a1a-a86e-4cf8-a923-33330a3f456b

---
+++ src/libxsp_modules/xsp_auth_ssl.h
@@ -0,0 +1,19 @@
+#ifndef XSP_AUTH_SSL_H
+#define XSP_AUTH_SSL_H
+
+//typedef struct xsp_ssl_user_info_t {
+//	char *username;
+//	char *password;
+//	char *email;
+//	char *institution;
+//	int activated;
+//} xspSSLUserInfo;
+
+//typedef struct xsp_ssl_backend_t {
+//	char *name;
+//	xspSSLUserInfo *(*get_user_info)(const char *username);
+//} xspSSLBackend;
+
+//int xsp_set_ssl_backend(xspSSLBackend *be);
+
+#endif
