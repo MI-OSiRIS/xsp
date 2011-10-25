a862dae74770e503275252e5ba8952070d8e6e3e
kissel <kissel@6dc24a1a-a86e-4cf8-a923-33330a3f456b>
2011-10-25 14:10:03 +0000

    client fixes
    git-svn-id: https://damsl.cs.indiana.edu/svn/xsp/trunk@204 6dc24a1a-a86e-4cf8-a923-33330a3f456b

--- src/libxsp_client/libxsp_client.h
+++ src/libxsp_client/libxsp_client.h
@@ -61,6 +61,7 @@ int xsp_send_ping(libxspSess *sess);
 int xsp_recv_ping(libxspSess *sess);
 int xsp_send_msg(libxspSess *sess, const void *buf, uint64_t len, int opt_type);
 int xsp_recv_msg(libxspSess *sess, void **ret_buf, uint64_t *len, int *ret_type);
+int xsp_wait_ack(libxspSess *sess);
 int xsp_sesscmp(libxspSess *s1, libxspSess *s2);
 
 #endif
