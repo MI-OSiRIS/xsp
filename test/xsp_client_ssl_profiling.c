2c323471a865e52bada8cb3a004f8ef72e22ff40
fernandes <fernandes@6dc24a1a-a86e-4cf8-a923-33330a3f456b>
2012-01-04 05:11:22 +0000

    Test clients for profiling xsp session establishment.
    Will add the netPath messages later.
    
    
    
    git-svn-id: https://damsl.cs.indiana.edu/svn/xsp/trunk@251 6dc24a1a-a86e-4cf8-a923-33330a3f456b

---
+++ test/xsp_client_ssl_profiling.c
@@ -0,0 +1,57 @@
+#include "config.h"
+
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <time.h>
+
+#include "option_types.h"
+
+#include "libxsp_client.h"
+
+/* Remember to set CLIENT_PEM and CLIENT_ROOT. */
+int main(int argc, char *argv[]) {
+    libxspSess *sess;
+    struct timeval t1, t2, t;
+
+    gettimeofday(&t1, NULL);
+    if (libxsp_init() < 0) {
+        perror("libxsp_init(): failed");
+        exit(errno);
+    }
+
+    sess = xsp_session();
+    if (!sess) {
+        perror("xsp_session() failed");
+        exit(errno);
+    }
+
+    xsp_sess_appendchild(sess, argv[argc - 1], XSP_HOP_NATIVE);
+
+    if (xsp_sess_set_security(sess, NULL, XSP_SEC_SSL)) {
+        fprintf(stderr, "could not set requested xsp security method\n");
+        exit(-1);
+    }
+
+    /* argc - 1 is the ultimate dest */
+    if (xsp_connect(sess)) {
+        perror("xsp_client: connect failed");
+        exit(errno);
+    }
+
+    gettimeofday(&t2, NULL);
+    timersub(&t2, &t1, &t);
+
+    printf("%f,", t.tv_sec + t.tv_usec / 1000000.0);
+    fflush(stdout);
+
+    xsp_close2(sess);
+
+    return 0;
+}
