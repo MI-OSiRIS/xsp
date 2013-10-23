8e2f8f6f770c132eb4b38b3dde8ad621c4348f29
Ezra Kissel <ezkissel@indiana.edu>
2013-10-23 15:33:02 -0400

    updates to the FSF sdn-test script

--- misc/py/fsf_paths.py
+++ misc/py/fsf_paths.py
@@ -10,32 +10,47 @@ import json
 #      (674)               (59066)
 #   (2) SW1 (676) -- (59590) SW2 (59491) 
 #     \                    /
-#       --  (1) SW3 (678) --
+#       --  (2) SW3 (674) --
 #
 
 hosts = {}
-hosts['sw1'] = {'mac': 'ff:ff:ff:ff:ff:ff', 'ip': ''}
-hosts['sw2'] = {'mac': 'ee:ee:ee:ee:ee:ee', 'ip': ''}
-
+hosts['pc1'] = {'mac': 'ff:ff:ff:ff:ff:ff', 'ip': '10.200.200.1', 'swport': '59066'}
+hosts['pc1_alt'] = {'mac': 'ff:ff:ff:ff:ff:ff', 'ip': '10.200.200.1', 'swport': '59066'}
+hosts['pc2'] = {'mac': 'ee:ee:ee:ee:ee:ee', 'ip': '10.200.200.2', 'swport': '674'}
+hosts['pc2_alt'] = {'mac': 'ee:ee:ee:ee:ee:ee', 'ip': '10.200.200.2', 'swport': '674'}
 
 switches = {}
-switches['sw1'] = {'dpid': '00:00:cc:4e:24:0c:08:00', 'hport': '674'}
-switches['sw2'] = {'dpid': '00:00:ac:4b:c8:41:ef:c0', 'hport': '59066'}
-switches['sw3'] = {'dpid': '00:00:cc:4e:24:0c:0c:00', 'hport': None}
+switches['sw1'] = {'dpid': '00:00:cc:4e:24:0c:08:00'}
+switches['sw2'] = {'dpid': '00:00:ac:4b:c8:41:ef:c0'}
+switches['sw3'] = {'dpid': '00:00:cc:4e:24:0c:0c:00'}
 
 paths = {}
-paths['sw1'] = {}
-paths['sw1']['sw2'] = {'route': ['sw1', 'sw2'], 'linkp': '676'}
-paths['sw1']['sw3'] = {'route': ['sw1', 'sw3'], 'linkp': '2'}
+paths['pc1'] = {}
+paths['pc1']['pc2'] = {'route': ['sw2', 'sw1']}
+paths['pc1']['pc2_alt'] = {'route': ['sw2', 'sw3', 'sw1']}
 
-paths['sw2'] = {}
-paths['sw2']['sw1'] = {'route': ['sw2', 'sw1'], 'linkp': '59590'}
-paths['sw2']['sw3'] = {'route': ['sw2', 'sw3'], 'linkp': '59491'}
+paths['pc2'] = {}
+paths['pc2']['pc1'] = {'route': ['sw1', 'sw2']}
+paths['pc2']['pc1_alt'] = {'route': ['sw1', 'sw3', 'sw2']}
 
-paths['sw3'] = {}
-paths['sw3']['sw1'] = {'route': ['sw3', 'sw1'], 'linkp': '1'}
-paths['sw3']['sw2'] = {'route': ['sw3', 'sw2'], 'linkp': '678'}
+nhops = {}
+nhops['sw1'] = {}
+nhops['sw1']['sw2'] = 676
+nhops['sw1']['pc1'] = 676
+nhops['sw1']['pc2'] = 674
+nhops['sw1']['sw3'] = 2
 
+nhops['sw2'] = {}
+nhops['sw2']['sw1'] = 59590
+nhops['sw2']['pc1'] = 59066
+nhops['sw2']['pc2'] = 59590
+nhops['sw2']['sw3'] = 59491
+
+nhops['sw3'] = {}
+nhops['sw3']['sw2'] = 674
+nhops['sw3']['pc1'] = 674
+nhops['sw3']['pc2'] = 2
+nhops['sw3']['sw1'] = 2
 
 class RestApi(object):
 
@@ -85,62 +100,61 @@ def make_json_flow(hop, srcm, dstm, inp, outp, vlan, i):
 def make_entries(src, dst, vlan, other):
     flows = []
     fiter = 0
-    hopc = 0
-    plen = len(paths[src][dst]['route'])
+    route = paths[src][dst]['route']
+    plen = len(route)
 
-    for hop in paths[src][dst]['route']:
+    for i in range(0, plen):
         # source
-        if (hop==src):
-            flows.append(make_json_flow(hop,
+        if (i==0):
+            flows.append(make_json_flow(route[i],
                                         hosts[src]['mac'],
                                         hosts[dst]['mac'],
-                                        switches[hop]['hport'],
-                                        paths[src][dst]['linkp'],
+                                        hosts[src]['swport'],
+                                        nhops[route[i]][route[i+1]],
                                         vlan,
                                         fiter))
-            flows.append(make_json_flow(hop,
+            flows.append(make_json_flow(route[i],
                                         hosts[dst]['mac'],
                                         hosts[src]['mac'],
-                                        paths[src][dst]['linkp'],
-                                        switches[hop]['hport'],
+                                        nhops[route[i]][route[i+1]],
+                                        hosts[src]['swport'],
                                         vlan,
                                         fiter+1))
         # destination
-        elif (hop==dst):
-            flows.append(make_json_flow(hop,
+        elif (i==(plen-1)):
+            flows.append(make_json_flow(route[i],
                                         hosts[src]['mac'],
                                         hosts[dst]['mac'],
-                                        paths[dst][src]['linkp'],
-                                        switches[hop]['hport'],
+                                        hosts[dst]['swport'],
+                                        nhops[route[i]][route[i-1]],
                                         vlan,
                                         fiter))
-            flows.append(make_json_flow(hop,
+            flows.append(make_json_flow(route[i],
                                         hosts[dst]['mac'],
                                         hosts[src]['mac'],
-                                        switches[hop]['hport'],
-                                        paths[dst][src]['linkp'],
+                                        nhops[route[i]][route[i-1]],
+                                        hosts[dst]['swport'],
                                         vlan,
                                         fiter+1))
         # we're some hop in the middle
         else:
-            flows.append(make_json_flow(hop,
+            flows.append(make_json_flow(route[i],
                                         hosts[src]['mac'],
                                         hosts[dst]['mac'],
-                                        paths[hop][src]['linkp'],
-                                        paths[hop][dst]['linkp'],
+                                        nhops[route[i]][route[i-1]],
+                                        nhops[route[i]][route[i+1]],
                                         vlan,
                                         fiter))
-            flows.append(make_json_flow(hop,
+            flows.append(make_json_flow(route[i],
                                         hosts[dst]['mac'],
                                         hosts[src]['mac'],
-                                        paths[hop][dst]['linkp'],
-                                        paths[hop][src]['linkp'],
+                                        nhops[route[i]][route[i+1]],
+                                        nhops[route[i]][route[i-1]],
                                         vlan,
                                         fiter+1))
 
 
         fiter+=2
-        hopc+=1
             
     return flows
 
@@ -167,14 +181,14 @@ rest = RestApi(args.ip, args.port)
 
 # get list of flow entries to push
 flows = make_entries(args.src, args.dst, args.vlan, args.otherargs)
-
+#
 #flows = [
-#    {'switch': '00:00:ac:4b:c8:41:ef:c0',
-#     'name': 'flow-mod-01',
+#    {'switch': '00:00:cc:4e:24:0c:0c:00',
+#     'name': 'flow-mod-97',
 #     'active': 'true',
-#     'ingress-port': 59066,
+#     'ingress-port': 678,
 #     'vlan-id': 4000,
-#     'actions': 'output=59590'
+#     'actions': 'output=676'
 #     }
 #    ]
 
