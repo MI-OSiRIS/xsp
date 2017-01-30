16e9b6b94f849d48a4e34382234dbc132c90ea38
Ezra Kissel <ezkissel@indiana.edu>
2017-01-30 14:59:35 -0500

    Pull libunis-c contrib using our standard make dist tarball.

--- autogen.sh
+++ autogen.sh
@@ -11,35 +11,52 @@
 #  Extreme Scale Technologies (CREST).
 # =============================================================================
 #!/bin/sh
+
 if [ ! -d config ]; then mkdir config; fi
-URL="https://github.com/periscope-ps/libunis-c.git"
+
+URL="http://stout.crest.iu.edu/xsp"
+DIR=$(mktemp -d xsp.XXXXXXX --tmpdir=/tmp)
 CONTRIB=contrib
-dir=libunis-c
-CMD_CLONE="git clone"
-CMD_PULL="git pull"
-if ! type "git" > /dev/null; then
-    echo -n "git not found please install git to proceed"
+
+# set the array of tarball deps here
+contrib_tars=("libunis-c-2.1.tar.bz2")
+# set matching md5sums for each tarball
+contrib_md5=("256b4c2b73da8caca357ae7670e6083a")
+# and matching target contrib directories
+contrib_dirs=("libunis-c")
+
+CMD="wget --quiet -O"
+if ! type "wget" > /dev/null; then
+    CMD="curl --silent -o"
 fi
-if [ ! -d ${CONTRIB}/$dir ]; then
-    echo "Cloning $dir"
-    $CMD_CLONE $URL ${CONTRIB}/$dir
-    if [ $? -ne 0 ]; then
-       echo "Error cloning $dir contrib"
-       continue
+echo -n "Downloading and extracting contrib tarballs..."
+for ((i=0; i<${#contrib_tars[@]}; i++)); do
+    dir=${contrib_dirs[$i]}
+    file=${contrib_tars[$i]}
+    md5=${contrib_md5[$i]}
+
+    if [ -f ${CONTRIB}/$dir/.md5 ]; then
+	omd5=`cat ${CONTRIB}/$dir/.md5`
+    else
+	omd5=""
     fi
-else
-    echo "Updating $dir - git pull"
-    cd ${CONTRIB}/$dir
-    $CMD_PULL
-    if [ $? -ne 0 ]; then
-       echo "Error updating $dir contrib"
-       continue
+
+    if [ ! -f ${CONTRIB}/$dir/configure ] || [ ! "$omd5" == "$md5" ]; then
+	echo -n "$dir..."
+	$CMD ${DIR}/$file ${URL}/$dir/$md5/$file
+	if [ $? -ne 0 ]; then
+	    echo
+	    echo "!!! Error downloading $dir contrib with md5sum $md5"
+	    continue
+	fi
+	mkdir -p ${CONTRIB}/$dir
+	tar --strip-components=1 -xf ${DIR}/$file -C ${CONTRIB}/$dir
+	echo -n $md5 > ${CONTRIB}/$dir/.md5
     fi
-    cd ../..
-fi
+done
+rm -rf ${DIR}
+echo "DONE"
+
 set -e
 autoreconf --force --install -I config || exit 1
 rm -rf autom4te.cache
-cd ${CONTRIB}/$dir
-./bootstrap.sh
-cd ../..
